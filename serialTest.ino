245f3c66ccb009de9d8e4daa3c404bd4f62e9c25
smart-life-tech <christlightworld@gmail.com>
2023-01-26 18:34:11 +0100

    modified:   serialTest.ino

--- serialTest.ino
+++ serialTest.ino
@@ -1,18 +1,18 @@
-#include <RBDdimmer.h> //
+#include <Dimmer.h> //
 
 // #define USE_SERIAL  SerialUSB //Serial for boards whith USB serial port
 #define USE_SERIAL Serial
-#define outputPin 12 // pwm pin
+#define outputPin 3 // pwm pin
 #define zerocross 2
 // dimmerLamp dimmer(outputPin, zerocross);
-dimmerLamp dimmer(outputPin); // initialase port for dimmer for MEGA, Leonardo, UNO, Arduino M0, Arduino Zero
-
+//dimmerLamp dimmer(outputPin); // initialase port for dimmer for MEGA, Leonardo, UNO, Arduino M0, Arduino Zero
+Dimmer dimmer(3, DIMMER_COUNT);
 int outVal = 0;
 
 void setup()
 {
     USE_SERIAL.begin(9600);
-    dimmer.begin(NORMAL_MODE, ON); // dimmer initialisation: name.begin(MODE, STATE)
+    dimmer.begin(); // dimmer initialisation: name.begin(MODE, STATE)
     USE_SERIAL.println("Dimmer Program is starting...");
     USE_SERIAL.println("Set value");
 }
@@ -37,16 +37,16 @@ void loop()
             outVal = buf;
             if(outVal>100)outVal=100;
         }
-        dimmer.setPower(outVal); // setPower(0-100%);
+        dimmer.set(outVal); // setPower(0-100%);
         delay(200);
     }
 
     if (preVal != outVal)
     {
-        dimmer.setPower(outVal); // setPower(0-100%);
+        dimmer.set(outVal); // setPower(0-100%);
         USE_SERIAL.print("lampValue -> ");
-        printSpace(dimmer.getPower());
-        USE_SERIAL.print(dimmer.getPower());
+        printSpace(outVal);
+       // USE_SERIAL.print(dimmer.getPower());
         USE_SERIAL.println("%");
         USE_SERIAL.println("Dimmer Program ...");
         USE_SERIAL.println("Set value from 0 to 100");
